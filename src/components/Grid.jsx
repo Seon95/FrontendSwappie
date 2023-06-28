28d6bd55d3935530b3297f86fad0c9305e87f18f
Marouan Ajtirah <marouan_39@hotmail.com>
2023-06-28 13:38:12 +0200

    grid

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -18,71 +18,63 @@ const Grid = ({ items, category }) => {
     navigate(`/detail/${itemId}`);
   };
 
-  return (
-    <Container fluid>
-      <Row
-        className="mt-4"
-        xs={1}
-        sm={2}
-        md={3}
-        lg={4}
-        xl={4}
-        gap={4}
-        // style={{ marginRight: "-100px" }}
-      >
-        {filteredItems.map((item) => {
-          const images = JSON.parse(item.images);
-          const filename =
-            images.length > 0 ? images[0].match(/[^_]+$/)[0] : "";
+  const renderItems = filteredItems.map((item) => {
+    const images = JSON.parse(item.images);
+    const filename = images.length > 0 ? images[0].match(/[^_]+$/)[0] : "";
 
-          console.log("filename:", filename); // Log the filename
-
-          return (
-            <Col key={item.id} className="mb-3">
-              <Card
+    return (
+      <Col key={item.id} className="mb-3" xs={12} sm={6} md={4} lg={4} xl={4}>
+        <Card
+          style={{
+            width: "250px",
+            height: "300px",
+            backgroundColor: "#e1e2e3",
+          }}
+        >
+          <Link to={`/detail/${item.id}`}>
+            {item.images && item.images.length > 0 ? (
+              <div
                 style={{
                   width: "250px",
-                  height: "300px",
-                  backgroundColor: "#e1e2e3",
+                  height: "250px",
+                  overflow: "hidden",
                 }}
               >
-                <Link to={`/detail/${item.id}`}>
-                  {item.images && item.images.length > 0 ? (
-                    <div
-                      style={{
-                        width: "250px",
-                        height: "250px",
-                        overflow: "hidden",
-                      }}
-                    >
-                      <Card.Img
-                        src={filename}
-                        alt={`image-${item.id}`}
-                        style={{
-                          width: "100%",
-                          height: "100%",
-                          objectFit: "cover",
-                        }}
-                      />
-                    </div>
-                  ) : (
-                    <div>No image available</div>
-                  )}
-                </Link>
-                <Card.Body>
-                  <Card.Text style={{ fontWeight: "600" }}>
-                    {item.name}{" "}
-                  </Card.Text>{" "}
-                  {/* Display the item description */}
-                  {/* <Button variant="primary" onClick={() => handleItemClick(item.id)}>
-                    View Details
-                  </Button> */}
-                </Card.Body>
-              </Card>
-            </Col>
-          );
-        })}
+                <Card.Img
+                  src={filename}
+                  alt={`image-${item.id}`}
+                  style={{
+                    width: "100%",
+                    height: "100%",
+                    objectFit: "cover",
+                  }}
+                />
+              </div>
+            ) : (
+              <div>No image available</div>
+            )}
+          </Link>
+          <Card.Body>
+            <Card.Text style={{ fontWeight: "600" }}>{item.name}</Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+    );
+  });
+
+  const rows = [];
+  for (let i = 0; i < renderItems.length; i += 3) {
+    const rowItems = renderItems.slice(i, i + 3);
+    rows.push(
+      <Row key={i} className="mt-4">
+        {rowItems}
       </Row>
+    );
+  }
+
+  return (
+    <Container fluid>
+      {rows}
       <Routes>
         <Route path="/detail/:id" element={<DetailPage />} />
       </Routes>
